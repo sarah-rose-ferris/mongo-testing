352d09d828d3048639ea2fa71fa3613f101d5ca6
Edward Hammersley <edham@netcompany.com>
2021-11-05 13:46:29 +0000

    copying funcitonality

--- src/test/groovy/com/example/mongotest/edMongoSpec.groovy
+++ src/test/groovy/com/example/mongotest/edMongoSpec.groovy
@@ -1,4 +1,45 @@
 package com.example.mongotest
 
-class edMongoSpec {
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class edMongoSpec extends Specification {
+
+    @Autowired
+    private helperFunctions helpers
+
+
+    def 'check customer is added'() {
+        given:
+        String firstName = "Ed"
+        String lastName = "Hammer"
+        int size = helpers.checkSize()
+
+        when:
+        helpers.addCustomerToDatabase(firstName,lastName)
+
+        then:
+        helpers.checkCustomerAdded(firstName)
+
+        and:
+        helpers.checkSize() == size+1
+
+
+    }
+
+    def 'check customer is deleted'() {
+        given:
+        String firstName = "SR"
+        String lastName = "Ferris"
+
+        when:
+        helpers.addCustomerToDatabase(firstName,lastName)
+        helpers.deleteCustomerFromDatabase(firstName)
+        Customer test = helpers.getCustomer(firstName)
+
+        then:
+        test == null
+    }
 }
